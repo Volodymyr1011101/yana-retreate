fef42b4cc673cd1451458bcf22a6974b51357cf5
Volodymyr Voronkin <you@example.com>
2024-09-18 20:56:06 +0200

    add details

--- src/components/whatIncludes/mock_data.ts
+++ src/components/whatIncludes/mock_data.ts
@@ -18,4 +18,4 @@ export const includes: string[] = [
     '2 plant-based meals daily (breakfast and dinner) with clean fresh voclano water and local fruits available daily at the Guest house',
     'Accommodation in a shared retreat house'
 ];
-export const exclusions: string[] = ['Transfer from/to Kona airport (we provide transter only from Hilo airport, please plan accordinaly', 'Personal expenses', 'Private body therapy from local massage therapist and contact singing bowls massage/soundbath by Yana Garden', 'Travel insurance (recommended)'] 
+export const exclusions: string[] = ['Transfer from/to Kona airport (we provide transter only from Hilo airport, please plan accordinaly', 'Personal expenses', 'Private body therapy from local massage therapist and contact singing bowls massage/soundbath by Yana Garden', 'Travel insurance (recommended)', 'Horse therapy'] 
